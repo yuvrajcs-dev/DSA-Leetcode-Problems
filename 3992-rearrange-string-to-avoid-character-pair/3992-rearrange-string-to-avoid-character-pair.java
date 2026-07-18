d377db9ef321060bafa4732b45dcc223aa0ed1c1
yuvrajcs-dev <ys4681986@gmail.com>
2026-07-18 23:58:06 +0530

    Time: 1 ms (100%), Space: 45.1 MB (85.62%) - LeetHub

---
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String rearrangeString(String s, char x, char y) {
+        char[] arr=s.toCharArray();
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==y){
+                char temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        return new String(arr);
+
+    }
+}
