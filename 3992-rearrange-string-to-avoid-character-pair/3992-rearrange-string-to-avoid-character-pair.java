2aef5ee2399726d4e813859de5e0f5e89edbdb99
yuvrajcs-dev <ys4681986@gmail.com>
2026-07-19 03:05:00 +0530

    Time: 1 ms (100%), Space: 45.3 MB (0%) - LeetHub

--- 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.java
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.java
@@ -4,13 +4,12 @@ class Solution {
         int j=0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]==y){
-                char temp=arr[i];
+                char tem=arr[i];
                 arr[i]=arr[j];
-                arr[j]=temp;
+                arr[j]=tem;
                 j++;
             }
         }
         return new String(arr);
-
     }
 }
