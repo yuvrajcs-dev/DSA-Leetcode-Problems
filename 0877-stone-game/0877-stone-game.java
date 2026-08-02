3fd585e4139c869c8012b8417e6f21f59ce716fb
yuvrajcs-dev <ys4681986@gmail.com>
2026-08-02 11:40:27 +0530

    Time: 10 ms (10.85%), Space: 49.7 MB (6.49%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    Integer[][] dp;
+    public boolean stoneGame(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n];
+
+        return solve(nums, 0, n - 1) >= 0;
+        
+    }
+    public int solve(int[] nums, int i, int j) {
+
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        int left = nums[i] - solve(nums, i + 1, j);
+
+        int right = nums[j] - solve(nums, i, j - 1);
+
+        return dp[i][j] = Math.max(left, right);
+    }
+}
