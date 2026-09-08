0b865366e140f24c62c0385d4fa751b39eee7414
Yuvraj Singh Shrinet <ys4681986@gmail.com>
2026-09-08 08:15:41 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (22.48%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int m) {
+        String str= String.valueOf(m);
+        int n=str.length();
+        if(n<4){
+            return 0;
+        }
+        int rem=m-999;
+        return rem;
+    }
+}
