116d4d5ce62585edbf8641432378296a9878c960
Yuvraj Singh Shrinet <ys4681986@gmail.com>
2026-09-19 07:32:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.83%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int i=n-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j=n-1;
+            while(nums[i]>=nums[j]){
+                j--;
+            }
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+        int left=i+1;
+        int right=n-1;
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
